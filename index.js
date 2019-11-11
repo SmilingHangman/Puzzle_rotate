d5b04d03baba31d9ad9c111bffe33549e3ef0702
SmilingHangman <andrejus.chomicenka@gmail.com>
2019-11-11 19:21:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// document.getElementsByClassName("rotate").addEventListener("click", function () {
+//     document.getElementsByClassName("rotate").style.transform = "rotate(90deg)"
+// }); iterate the array
+
+document.getElementById("squareOne").addEventListener("click", function () {
+    document.getElementById("squareOne").style.transform += "rotate(90deg)";
+});
+
+document.getElementById("squareTwo").addEventListener("click", function () {
+    document.getElementById("squareTwo").style.transform += "rotate(90deg)";
+});
+
+document.getElementById("squareThree").addEventListener("click", function () {
+    document.getElementById("squareThree").style.transform += "rotate(90deg)";
+});
+
+document.getElementById("squareFour").addEventListener("click", function () {
+    document.getElementById("squareFour").style.transform += "rotate(90deg)";
+});
+
+document.getElementById("squareFive").addEventListener("click", function () {
+    document.getElementById("squareFive").style.transform += "rotate(90deg)";
+});
+
+document.getElementById("squareSix").addEventListener("click", function () {
+    document.getElementById("squareSix").style.transform += "rotate(90deg)";
+});
+
+document.getElementById("squareSeven").addEventListener("click", function () {
+    document.getElementById("squareSeven").style.transform += "rotate(90deg)";
+});
+
+document.getElementById("squareEight").addEventListener("click", function () {
+    document.getElementById("squareEight").style.transform += "rotate(90deg)";
+});
+
+document.getElementById("squareNine").addEventListener("click", function () {
+    document.getElementById("squareNine").style.transform += "rotate(90deg)";
+});
