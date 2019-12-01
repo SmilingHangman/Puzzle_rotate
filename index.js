c31b26554ad84768ff931dc66b409146306c19cf
SmilingHangman <andrejus.chomicenka@gmail.com>
2019-12-02 00:07:59 +0200

    relocated shuffle button

--- index.js
+++ index.js
@@ -5,17 +5,16 @@
 let rngRotator = Math.floor(((Math.random() * 3) + 5) * 90);
 
 document.getElementById("shuffle").addEventListener("click", function () {
-    document.getElementById("squareOne").style.transform += "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
-    document.getElementById("squareTwo").style.transform += "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
-    document.getElementById("squareThree").style.transform += "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
-    document.getElementById("squareFour").style.transform += "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
-    document.getElementById("squareFive").style.transform += "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
-    document.getElementById("squareSix").style.transform += "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
-    document.getElementById("squareSeven").style.transform += "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
-    document.getElementById("squareEight").style.transform += "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
-    document.getElementById("squareNine").style.transform += "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
+    document.getElementById("squareOne").style.transform = "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
+    document.getElementById("squareTwo").style.transform = "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
+    document.getElementById("squareThree").style.transform = "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
+    document.getElementById("squareFour").style.transform = "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
+    document.getElementById("squareFive").style.transform = "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
+    document.getElementById("squareSix").style.transform = "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
+    document.getElementById("squareSeven").style.transform = "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
+    document.getElementById("squareEight").style.transform = "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
+    document.getElementById("squareNine").style.transform = "rotate(" + String((Math.floor(Math.random() * 10 + 1)) * 90) + "deg)";
 });
-
 
 document.getElementById("squareOne").addEventListener("click", function () {
     document.getElementById("squareOne").style.transform += "rotate(90deg)";
